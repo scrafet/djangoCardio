e97533f1abb32bed4cfe7cf5954bea00585d6577
scrafet <scrafet@gmail.com>
2024-08-04 16:34:57 -0500

    primera carga

---
+++ djangoCardio/Web/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def homeView(request):
+    return render(request, 'home.html')
+# age
+# gender
+# height
+# weight
+# ap_hi          
+# ap_lo
+# cholesterol
+# gluc
+# smoke
+# alco
+# active
+# cardio
+# bmi
+
+def resultsView(request):
+    pass
